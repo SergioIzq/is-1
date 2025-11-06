9656aa3574751bd17c6415a6544e46b4ff058265
Sergio Izquierdo <sergioizq.dev@gmail.com>
2025-11-06 13:47:15 +0100

    Añado clases al modelo

---
+++ ZooApp/src/modelo/Empleado.java
@@ -0,0 +1,51 @@
+package modelo;
+
+/**
+ *
+ * @author Sergio Izquierdo, Isabel Cherino, Sandra Megolla
+ */
+public class Empleado {
+    private String nombre;
+    private String usuario;
+    private String contrasena;
+    private String rol;
+
+    public Empleado(String nombre, String usuario, String contrasena, String rol) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
--- ZooApp/src/modelo/Visitante.java
+++ ZooApp/src/modelo/Visitante.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 /**
  *
- * @author eps
+ * @author Sergio Izquierdo, Isabel Cherino, Sandra Megolla
  */
 public class Visitante {
-    
+
+    private String nombre;
+    private String email;
+    private String telefono;
+
+    public Visitante(String nombre, String email, String telefono) {
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        // TODO --> COMPROBAR QUE TENGA FORMATO EMAIL
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        // TODO --> COMPROBAR QUE TENGA FORMATO TELF
+        this.telefono = telefono;
+    }
+
 }
