e84f9536c052b1120866c1bfe0607ff67db6389f
SandraMegolla <135312327+SandraMegolla@users.noreply.github.com>
2025-11-06 14:49:07 +0100

    Update Sistema.java

--- ZooApp/src/modelo/Sistema.java
+++ ZooApp/src/modelo/Sistema.java
@@ -4,10 +4,100 @@
  */
 package modelo;
 
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+import modelo.Utils.TipoEnum;
+
 /**
  *
  * @author eps
  */
 public class Sistema {
+    //CASO USO 1
+    public static void accederSistema(){
+        
+    }   
+    public static void mostrarOpcionesEntrada(){
+        
+    }
+    public static void seleccionarEntradas(TipoEnum m,Date fecha,double precio){
+        
+    }
+    public static void mostrarResumenCompra(){
+        
+    }
+    public static void introducirDatosPago(){
+        
+    }
+    public static void validarPago(){
+        
+    }
     
+    //CASO USO 2
+    public static void mostrarListaEspecies(){
+        
+    }
+    public static void listaEspecies(){
+        
+    }
+    public static void buscarEspecie(String nombreComun){
+        
+    }
+    public static void mostrarDatosEspecies(){
+        
+    }
+    
+    //CASO USO 3
+    public static void iniciarSesionEmpleado(String usuario,String contrasena){
+        
+    }
+    public static void mostrarPanelAdministracion(){
+        
+    }
+    public static void anadirEspecie(){
+        
+    }
+    public static void mostrarFormularioEspecie(){
+        
+    }
+    public static void introducirDatosEspecie(String nombreComun,String nombreCientifico, List<String> alimentacion,String estadoConservacion,List<String> curiosidades ){
+        
+    }
+    public static void guardarCambios(){
+        
+    }
+    public static void mostrarMensajeAdd(){
+        
+    }
+    
+    //CASO USO 4
+    
+    public static void mostrarPanelGestion(){
+        
+    }
+    public static void seleccionarGestionHorarios(){
+        
+    }
+    public static void mostrarHorariosActuales(){
+        
+    }
+    public static void modificarHorarios(String diaSemana,LocalTime horaApertura,LocalTime horaCierre){
+        
+    }
+    public static void mostrarConfirmacion(){
+        
+    }
+    
+    //CASO USO 5
+    
+    public static void accederModuloReportes(){
+        
+    }
+    public static void mostrarOpcionesReporte(){
+        
+    }
+    public static void seleccionarTipoReporte(){
+        
+    }
 }
